68a3186542e06bfedc01fc7cf87bd2e178c07595
Jose Armando <joseearmandoo28@gmail.com>
2026-04-24 09:01:09 +0700

    remove create default user for seeder

--- database/seeders/UserRolePermissionV2Seeder.php
+++ database/seeders/UserRolePermissionV2Seeder.php
@@ -16,57 +16,58 @@ class UserRolePermissionV2Seeder extends Seeder
      */
     public function run(): void
     {
-        $viewDrawingTransactionPermission = Permission::where('name', 'view_drawing_transaction')->first();
-        $rejectDrawingTransactionPermission = Permission::where('name', 'reject_drawing_transaction')->first();
-
+        
         $bomApprovePermission = Permission::updateOrCreate(
             ['name' => 'bom_approve_distributed_drawing_transaction', 'guard_name' => 'web'],
             ['ui_name' => 'Can BOM Approve Distributed Drawing Transaction']
         );
-
+            
         $costingApprovePermission = Permission::updateOrCreate(
             ['name' => 'costing_approve_distributed_drawing_transaction', 'guard_name' => 'web'],
             ['ui_name' => 'Can Costing Approve Distributed Drawing Transaction']
         );
+                
+        // $viewDrawingTransactionPermission = Permission::where('name', 'view_drawing_transaction')->first();
+        // $rejectDrawingTransactionPermission = Permission::where('name', 'reject_drawing_transaction')->first();
 
-        $bomRole = Role::firstOrCreate(
-            ['name' => 'BOM', 'guard_name' => 'web']
-        );
+        // $bomRole = Role::firstOrCreate(
+        //     ['name' => 'BOM', 'guard_name' => 'web']
+        // );
 
-        $bomRole->syncPermissions([
-            $viewDrawingTransactionPermission,
-            $rejectDrawingTransactionPermission,
-            $bomApprovePermission,
-        ]);
+        // $bomRole->syncPermissions([
+        //     $viewDrawingTransactionPermission,
+        //     $rejectDrawingTransactionPermission,
+        //     $bomApprovePermission,
+        // ]);
 
-        $costingRole = Role::firstOrCreate(
-            ['name' => 'Costing', 'guard_name' => 'web']
-        );
+        // $costingRole = Role::firstOrCreate(
+        //     ['name' => 'Costing', 'guard_name' => 'web']
+        // );
 
-        $costingRole->syncPermissions([
-            $viewDrawingTransactionPermission,
-            $rejectDrawingTransactionPermission,
-            $costingApprovePermission,
-        ]);
+        // $costingRole->syncPermissions([
+        //     $viewDrawingTransactionPermission,
+        //     $rejectDrawingTransactionPermission,
+        //     $costingApprovePermission,
+        // ]);
 
-        $bomUser = User::updateOrCreate(
-            ['email' => 'bom@email.com'],
-            [
-                'name' => 'General Account (BOM)',
-                'password' => bcrypt('password'),
-            ]
-        );
+        // $bomUser = User::updateOrCreate(
+        //     ['email' => 'bom@email.com'],
+        //     [
+        //         'name' => 'General Account (BOM)',
+        //         'password' => bcrypt('password'),
+        //     ]
+        // );
 
-        $bomUser->syncRoles([$bomRole]);
+        // $bomUser->syncRoles([$bomRole]);
 
-        $costingUser = User::updateOrCreate(
-            ['email' => 'costing@email.com'],
-            [
-                'name' => 'General Account (Costing)',
-                'password' => bcrypt('password'),
-            ]
-        );
+        // $costingUser = User::updateOrCreate(
+        //     ['email' => 'costing@email.com'],
+        //     [
+        //         'name' => 'General Account (Costing)',
+        //         'password' => bcrypt('password'),
+        //     ]
+        // );
 
-        $costingUser->syncRoles([$costingRole]);
+        // $costingUser->syncRoles([$costingRole]);
     }
 }
